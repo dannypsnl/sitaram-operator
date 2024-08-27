0fac69459d5a62be3cb0bc6087a2cf8065e4033b
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-27 10:43:36 +0800

    rename
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- raise-and-log.rkt
+++ raise-and-log.rkt
@@ -1,43 +1,44 @@
 #lang racket
-(require racket/control)
+(require racket/control
+         (for-syntax syntax/parse))
 
 (define raise-tag (make-continuation-prompt-tag 'raise))
 (define (raise e) (fcontrol e #:tag raise-tag))
 
 (define-syntax (try stx)
-  (syntax-case stx ()
+  (syntax-parse stx
     [(_ body handler)
      #'(% body
           (λ (e _)
             (handler e))
           #:tag raise-tag)]))
 
-(define (f2)
-  (println "f2 do something")
+(define (f)
+  (println "f do something")
   (raise 'fail))
-(try (f2)
+(try (f)
      (λ (err)
-       (println "f2 failed")
+       (println "f failed")
        (println err)))
 
 
 (define log-tag (make-continuation-prompt-tag 'log))
 (define (log message) (fcontrol message #:tag log-tag))
 (define-syntax (with-log stx)
-  (syntax-case stx ()
-    [(_ body handler)
+  (syntax-parse stx
+    [(with-log body handler)
      #'(% body
           (λ (msg resume)
             (handler msg)
             (resume))
           #:tag log-tag)]))
 
-(define (f3)
-  (log "f3 do something")
+(define (g)
+  (log "g do something")
   (raise 'fail))
-(try (with-log (f3)
+(try (with-log (g)
        (λ (msg)
          (println msg)))
      (λ (err)
-       (println "f2 failed")
+       (println "g failed")
        (println err)))
